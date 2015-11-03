30bb640160cb2efe26a4a80b717acd6d7dbeedf2
njk28 <njk28@students.calvin.edu>
2015-11-03 11:41:43 -0500

    Update queries1.sql

--- queries1.sql
+++ queries1.sql
@@ -1,12 +1,18 @@
 --search
-SELECT name FROM Recipe WHERE Name LIKE “%UserInput%” ORDER BY name
+SELECT name 
+FROM Recipe 
+WHERE name LIKE “%UserInput%” 
+ORDER BY name
 
 --get search results
 SELECT Recipe.name, Recipe.prepInstructions, Note.content, Ingredient.name, RI.unit, RI.quantity 
 FROM Recipe, Ingredient, RI, Note 
-WHERE Note.recipeID = Recipe.ID AND RI.recipeID = Recipe.ID AND Ingredient.ID = RI.ingredientID AND Recipe.ID = 3 --Recipe will be determined by user after selection
+WHERE Note.recipeID = Recipe.ID AND RI.recipeID = Recipe.ID AND 
+  Ingredient.ID = RI.ingredientID AND Recipe.ID = 3 --Recipe will be determined by user after selection
+
 --get shopping list
 SELECT  RI.quantity, RI.unit, Ingredient.name, Ingredient.type
 FROM Ingredient, RI, Recipe, Dish
-WHERE RI.recipeID = Recipe.ID AND RI.ingredientID = Ingredient.ID AND Dish.recipeID = Recipe.ID AND Dish.date >= (current_date + interval '7 days')
+WHERE RI.recipeID = Recipe.ID AND RI.ingredientID = Ingredient.ID AND Dish.recipeID = Recipe.ID 
+  AND Dish.date >= (current_date + interval '7 days')
 ORDER BY Ingredient.type;
